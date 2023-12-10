224b7e5c2f6531885ef2ec9c7808b3fc29951b7a
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-12-10 21:56:55 +0530

    trying to retrieve the assistant with threads

--- backend/assistant.py
+++ backend/assistant.py
@@ -3,7 +3,6 @@ import shelve
 from dotenv import load_dotenv
 import os
 import time
-
 load_dotenv()
 
 OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
@@ -155,27 +154,20 @@ code = """
  </body>
 </html>
 """
-
 # --------------------------------------------------------------
-# Create or Retrieve assistant
+# Create assistant
 # --------------------------------------------------------------
-def create_or_retrieve_assistant():
-    try:
-        assistant = client.beta.assistants.retrieve(assistants_id)
-        print(f"Using existing assistant with id: {assistants_id}")
-    except Exception as e:
-        print(f"Error retrieving existing assistant: {e}")
-        assistant = client.beta.assistants.create(
-            name="Automation code Generator",
-            instructions="Write Java Selenium test cases covering edge cases, assertions on page elements, and ensure reusability with the Page Object Model for provided HTML.``` Separate responses for different test cases by creating test case method without additional context, import statements, or before/after class methods. for the below html code assuming that web driver has already been created```",
-            tools=[{"type": "code_interpreter"}],
-            model="gpt-3.5-turbo-16k"
-        )
-        print(f"Created new assistant with id: {assistant.id}")
+def create_assistant():
+    assistant = client.beta.assistants.create(
+        name="Automation code Generator",
+        instructions="Write Java Selenium test cases covering edge cases, assertions on page elements, and ensure reusability with the Page Object Model for provided HTML.``` Separate responses for different test cases by creating test case method without additional context, import statements, or before/after class methods. for the below html code assuming that web driver has already been created```",
+        tools=[{"type": "code_interpreter"}],
+        model="gpt-3.5-turbo-16k"
+    )
     return assistant
 
-assistant = create_or_retrieve_assistant()
-assistants_id = assistant.id  # Update the assistant ID
+assistant = create_assistant()
+
 
 # --------------------------------------------------------------
 # Thread management
@@ -184,9 +176,11 @@ def check_if_thread_exists(wa_id):
     with shelve.open("threads_db") as threads_shelf:
         return threads_shelf.get(wa_id, None)
 
+
 def store_thread(wa_id, thread_id):
     with shelve.open("threads_db", writeback=True) as threads_shelf:
         threads_shelf[wa_id] = thread_id
+
 
 # --------------------------------------------------------------
 # Generate response
@@ -197,7 +191,7 @@ def generate_response(message_body, wa_id, name):
 
     # If a thread doesn't exist, create one and store it
     if thread_id is None:
-        print(f"Creating new thread for {name} with wa_id {wa_id}")
+        print(f"Creating new thread for {name} with wa_id")
         thread = client.beta.threads.create()
         store_thread(wa_id, thread.id)
         thread_id = thread.id
@@ -205,6 +199,7 @@ def generate_response(message_body, wa_id, name):
     # Otherwise, retrieve the existing thread
     else:
         print("Thread exists..")
+        thread = client.beta.threads.retrieve(thread_id)
 
     # Add message to thread
     message = client.beta.threads.messages.create(
@@ -218,10 +213,14 @@ def generate_response(message_body, wa_id, name):
     print(f"To {name}:", new_message)
     return new_message
 
+
 # --------------------------------------------------------------
 # Run assistant
 # --------------------------------------------------------------
 def run_assistant(thread):
+    # Retrieve the Assistant
+    assistant = client.beta.assistants.retrieve(assistants_id)
+
     # Run the assistant
     run = client.beta.threads.runs.create(
         thread_id=thread.id,
@@ -240,8 +239,10 @@ def run_assistant(thread):
     print(f"Generated message: {new_message}")
     return new_message
 
+
 # --------------------------------------------------------------
 # Test assistant
 # --------------------------------------------------------------
+
 new_message = generate_response(code, assistants_id, "Junior Vagrants")
 print(new_message)
