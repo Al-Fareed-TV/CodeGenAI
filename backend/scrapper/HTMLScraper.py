502b611543c2cf115e1f254fc193525f56e4e025
Sharat Hegde <sharath@testvagrant.com>
2023-12-10 19:20:31 +0530

    simple implementation of webscraping

---
+++ backend/scrapper/HTMLScraper.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+def save_html(url, output_file="output.html"):
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            with open(output_file, 'w', encoding='utf-8') as file:
+                file.write(soup.prettify())
+                
+            print(f"HTML content saved to {output_file}")
+
+        else:
+            print(f"Failed to retrieve HTML. Status code: {response.status_code}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# Example usage
+url_to_scrape = "https://rahulshettyacademy.com/loginpagePractise/"
+save_html(url_to_scrape, output_file="output.html")
