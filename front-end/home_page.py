3301f3bc669476c21cc6cc096dbded191699150b
Baganna Kumbar <bagannak@testvagrant.com>
2023-12-10 22:44:56 +0530

    added styling to text input, select box and submit button

--- front-end/home_page.py
+++ front-end/home_page.py
@@ -5,11 +5,23 @@ class Home_Page:
         st.title("Auto TestCase Generator")
 
         # Input textbox with styling
-    
+        st.markdown("""
+        <style>
+            div.Widget.row-widget.stTextInput > div {
+                width: 300px;
+            }
+        </style>
+    """, unsafe_allow_html=True)
         input_text = st.text_input("Enter URL:", "")
 
         # Dropdown with styling
-
+        st.markdown("""
+        <style>
+            div.Widget.row-widget.stSelectbox >div{
+                width: 300px;
+            }
+        </style>
+    """, unsafe_allow_html=True)
         tools = ["Select","Selenium", "PlayWright", "WebDriverIO"]
         selected_tool = st.selectbox("Select an tool:", tools)
         if selected_tool == "Selenium":
@@ -22,7 +34,17 @@ class Home_Page:
             languages = ["Select", "JavaScript"]
             selected_language = st.selectbox("Select an Language:", languages)
         # Submit button with styling
-    
+        st.markdown("""
+        <style>
+            div.Widget.stButton > button {
+                width: 150px;
+                height: 40px;
+                background-color: #4CAF51;
+                color: white;
+                font-size: 16px;
+            }
+        </style>
+    """, unsafe_allow_html=True)
         if st.button("Generate Testcase"):
             if not input_text:
                 st.warning("Please enter URL.")
