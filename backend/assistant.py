3009af3f29bc97eaa557da96677eac9bec25f20c
TestVagrant <testvagrant@Sudhindras-MacBook-Pro.local>
2023-12-10 21:35:11 +0530

    created assistant, and it can generate response

--- backend/assistant.py
+++ backend/assistant.py
@@ -57,7 +57,33 @@ def store_thread(wa_id, thread_id):
 # --------------------------------------------------------------
 # Generate response
 # --------------------------------------------------------------
+def generate_response(message_body, wa_id, name):
+    # Check if there is already a thread_id for the wa_id
+    thread_id = check_if_thread_exists(wa_id)
 
+    # If a thread doesn't exist, create one and store it
+    if thread_id is None:
+        print(f"Creating new thread for {name} with wa_id {wa_id}")
+        thread = client.beta.threads.create()
+        store_thread(wa_id, thread.id)
+        thread_id = thread.id
+
+    # Otherwise, retrieve the existing thread
+    else:
+        print(f"Retrieving existing thread for {name} with wa_id {wa_id}")
+        thread = client.beta.threads.retrieve(thread_id)
+
+    # Add message to thread
+    message = client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=message_body,
+    )
+
+    # Run the assistant and get the new message
+    new_message = run_assistant(thread)
+    print(f"To {name}:", new_message)
+    return new_message
 
 
 # --------------------------------------------------------------
