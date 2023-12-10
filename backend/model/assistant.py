328ad8b0de4b8a846de3959703e9b6d7390a8075
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-12-10 22:43:38 +0530

    modified generate respone method to call scrapper

--- backend/model/assistant.py
+++ backend/model/assistant.py
@@ -1,12 +1,14 @@
 from openai import OpenAI
 import shelve
 from dotenv import load_dotenv
-import os
+import os,sys
 import time
 load_dotenv()
 OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
 client = OpenAI(api_key=OPEN_AI_API_KEY)
 assistants_id = os.getenv("assistant_id")
+sys.path.append("/Users/testvagrant/Documents/junior-vagrants/backend/")
+from scrapper.HTMLScraper import HTMLScraper
 # --------------------------------------------------------------
 # Get the code
 # --------------------------------------------------------------
@@ -54,33 +56,24 @@ def store_thread(wa_id, thread_id):
 # --------------------------------------------------------------
 # Generate response
 # --------------------------------------------------------------
-def generate_response(message_body, wa_id, name):
-    # Check if there is already a thread_id for the wa_id
-    thread_id = check_if_thread_exists(wa_id)
-
-    # If a thread doesn't exist, create one and store it
-    if thread_id is None:
-        print(f"Creating new thread for {name} with wa_id")
-        thread = client.beta.threads.create()
-        store_thread(wa_id, thread.id)
+def generate_response(self,url):
+        scrapper = HTMLScraper()
+        htmlCode = scrapper.remove_css_js_and_save_html(url)
+        # If a thread doesn't exist, create one and store it
+        print(f"Creating new thread")
+        thread = self.client.beta.threads.create()
         thread_id = thread.id
 
-    # Otherwise, retrieve the existing thread
-    else:
-        print("Thread exists..")
-        thread = client.beta.threads.retrieve(thread_id)
-
-    # Add message to thread
-    message = client.beta.threads.messages.create(
-        thread_id=thread_id,
-        role="user",
-        content=message_body,
-    )
-
-    # Run the assistant and get the new message
-    new_message = run_assistant(thread)
-    print(f"To {name}:", new_message)
-    return new_message
+        # Add message to thread
+        message = self.client.beta.threads.messages.create(
+            thread_id=thread_id,
+            role="user",
+            content=htmlCode,
+        )
+        # Run the assistant and get the new message
+        new_message = run_assistant(thread)
+        print(f"To:", new_message)
+        return new_message
 
 
 # --------------------------------------------------------------
